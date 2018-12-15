9bf080229f730ad504912a41578a5a689aab1bf2
Elena Mazurenko <mazurenko.olena@pdffiller.team>
2018-12-16 00:29:02 +0200

    Added additional spaces

--- 4.2_Print_snake.php
+++ 4.2_Print_snake.php
@@ -12,9 +12,9 @@ $start = 1;
 for ($i = 0; $i < $size; $i++) {
     for ($j = 0; $j < $size; $j++) {
         if ($i % 2 == 0) {
-            $myArray[$i][$j] = $start++;
+            $myArray[$i][$j] = $start++ . ' ';
         } else {
-            $myArray[$i][$size - $j - 1] = $start++;
+            $myArray[$i][$size - $j - 1] = $start++ . ' ';
         }
     }
 
